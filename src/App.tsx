64c08f75008d25b773fad098a862187f6bad4efa
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-22 22:59:57 -0300

    feat: implementing shacn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Button } from './components/ui/button'
 import './global.css'
 
 export function App() {
   return (
-    <>
-    <h1>Hello world</h1>
-    </>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>
+        enviar
+      </Button>
+    </div>
   )
 }
 
