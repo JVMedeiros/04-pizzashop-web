a73d0923718d0366e121e7cbbb78b92f98d8c220
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-22 23:09:34 -0300

    feat: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>
-        enviar
-      </Button>
+    <div className="flex h-screen items-center justify-center">
+      <Button>enviar</Button>
     </div>
   )
 }
