11ddbb495c359e286810276375056fa2b22a29e4
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-21 23:36:37 -0300

    feat: page tittle add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './global.css'
 
+import  {HelmetProvider, Helmet } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+
 import { router } from './routes'
 
 export function App() {
   return (
-    <RouterProvider router={router}/>
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | pizza.shop'/>
+      <RouterProvider router={router}/>
+    </HelmetProvider>
   )
 }
 
