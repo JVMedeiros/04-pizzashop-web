21c4e8d4da2a1bd42bfad83b81879f2ffa1f2f74
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-23 21:24:08 -0300

    feat: add sign up page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
 import { AppLayout } from "./pages/_layout/app";
 import { AuthLayout } from "./pages/_layout/auth";
 
 export const router = createBrowserRouter([
   {
-    path: '/', 
-    element: <AppLayout />, 
-    children: [{path: '/', element: <Dashboard />}]
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
   },
   {
-    path: '/sign-in', 
-    element: <AuthLayout />, 
-    children: [{path: '/sign-in', element: <SignIn />}]
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+  ]
   },
 ])
