12b8cf505b6581a3ecd591be6cde501e8e9c7d6c
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-25 04:01:42 -0300

    fix: eslint fix

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,37 +1,38 @@
-import { Button } from "@/components/ui/button";
-import { TableRow, TableCell } from "@/components/ui/table";
-import { ArrowRight, X, Search } from "lucide-react";
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
         <Button variant="outline" size="xs">
-          <Search className="h-3 w-3"/>
+          <Search className="h-3 w-3" />
           <span className="sr-only">Detalhes do pedido</span>
-        </Button> 
+        </Button>
       </TableCell>
-      <TableCell className="font-mono text-xs font-medium">8794656-asad64553</TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        8794656-asad64553
+      </TableCell>
       <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
       <TableCell>
         <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400"/>
-          <span className="font-medium text-muted-foreground">
-            Pendente
-          </span>
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
         </div>
       </TableCell>
       <TableCell className="font-medium">João Medeiros</TableCell>
       <TableCell className="font-medium">R$ 149,90</TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
-          <ArrowRight className="mr-2 h-3 w-3"/>
+          <ArrowRight className="mr-2 h-3 w-3" />
           Aprovar
         </Button>
       </TableCell>
       <TableCell>
         <Button variant="ghost" size="xs">
-          <X className="mr-2 h-3 w-3"/>
+          <X className="mr-2 h-3 w-3" />
           Cancelar
         </Button>
       </TableCell>
