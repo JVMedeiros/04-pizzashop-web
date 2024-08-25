d17012c8566777e15832181db83eb52a93ffea63
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-25 03:59:52 -0300

    fix: eslint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './global.css'
 
-import  {HelmetProvider, Helmet } from 'react-helmet-async'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
-
-import { router } from './routes'
 import { Toaster } from 'sonner'
+
 import { ThemeProvider } from './components/theme/theme-provider'
+import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
-        <Helmet titleTemplate='%s | pizza.shop'/>
-        <Toaster richColors/>
-        <RouterProvider router={router}/>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </HelmetProvider>
   )
