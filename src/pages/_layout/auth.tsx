dbc5964c1fedaa523cd86ae6a24f025c97236ce6
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-25 04:01:23 -0300

    fix: eslint fix

--- src/pages/_layout/auth.tsx
+++ src/pages/_layout/auth.tsx
@@ -1,10 +1,10 @@
-import { Outlet } from "react-router-dom";
-import { Pizza } from 'lucide-react' 
+import { Pizza } from 'lucide-react'
+import { Outlet } from 'react-router-dom'
 
-export function AuthLayout () {
+export function AuthLayout() {
   return (
-    <div className="min-h-screen grid grid-cols-2 antialiased">
-      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="h-5 w-5" />
           <span className="font-semibold">pizza.shop</span>
@@ -14,7 +14,7 @@ export function AuthLayout () {
         </footer>
       </div>
 
-      <div className="flex flex-col items-center justify-center relative">
+      <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
