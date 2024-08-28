1b3adfe11f07845154b5295409f861b794621f2d
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-28 15:40:03 -0300

    feat: signin func wip

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -7,6 +7,8 @@ import { z } from 'zod'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useMutation } from '@tanstack/react-query'
+import { signIn } from '@/api/sign-in'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -21,16 +23,24 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
-  async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn
+  })
 
-    toast.success('Enviamos um link de autenticação para o seu e-mail.', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => handleSignIn(data),
-      },
-    })
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await authenticate({email: data.email})
+
+      toast.success('Restaurante cadastrado com sucesso!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+      
+    } catch (error) {
+      toast.error('Erro ao cadastrar restaurante.')
+    }
   }
 
   return (
