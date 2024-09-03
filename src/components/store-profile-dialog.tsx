e62bea225cc15ce8022530ba25fab728c3704c64
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-02 23:33:10 -0300

    feat: implementing update user info when necessary

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,5 +1,5 @@
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -19,6 +19,7 @@ const storeProfileSchema = z.object({
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
 
 export function StoreProfileDialog() {
+  const queryClient = useQueryClient()
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant  } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
@@ -34,7 +35,18 @@ export function StoreProfileDialog() {
   })
 
   const {mutateAsync: updateProfileFn } = useMutation({
-    mutationFn: UpdateProfile
+    mutationFn: UpdateProfile,
+    onSuccess(_, {name, description}) {
+      const cached = queryClient.getQueryData(['managed-restaurant'])
+
+      if (cached) {
+        queryClient.setQueryData(['managed-restaurant'], {
+          ...cached, 
+          name, 
+          description
+        })
+      }
+    },
   })
 
   async function handleUpdateProfile(data: StoreProfileSchema) {
