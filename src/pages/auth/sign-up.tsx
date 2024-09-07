7f2b8b596890bedc0108e9cd38b853a6a5f525af
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-07 14:20:57 -0300

    fix: eslint fix

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,14 +1,14 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { useMutation } from '@tanstack/react-query'
-import { registerRestaurant } from '@/api/register-restaurant'
 
 const signUpForm = z.object({
   restaurantName: z.string(),
@@ -29,7 +29,7 @@ export function SignUp() {
   } = useForm<SignUpForm>()
 
   const { mutateAsync: registerRestaurantFn } = useMutation({
-    mutationFn: registerRestaurant
+    mutationFn: registerRestaurant,
   })
 
   async function handleSignUp(data: SignUpForm) {
@@ -37,10 +37,10 @@ export function SignUp() {
       console.log(data)
 
       await registerRestaurantFn({
-          restaurantName: data.restaurantName,
-          managerName: data.managerName,
-          email: data.email,
-          phone: data.phone,
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
       })
 
       toast.success('Restaurante cadastrado com sucesso!', {
