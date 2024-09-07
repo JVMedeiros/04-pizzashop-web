d23748a662d5a89c5d0ba2ba230a349c98967a8b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-07 14:15:02 -0300

    fix: eslint fix

--- src/api/get-day-orders-amount.ts
+++ src/api/get-day-orders-amount.ts
@@ -1,4 +1,4 @@
-import { api } from "@/lib/axios";
+import { api } from '@/lib/axios'
 
 export interface GetDayOrdersAmountResponse {
   amount: number
@@ -6,7 +6,9 @@ export interface GetDayOrdersAmountResponse {
 }
 
 export async function getDayOrdersAmount() {
-  const response = await api.get<GetDayOrdersAmountResponse>('/metrics/day-orders-amount')
+  const response = await api.get<GetDayOrdersAmountResponse>(
+    '/metrics/day-orders-amount',
+  )
 
   return response.data
 }
