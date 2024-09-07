a2aaa1a91d6cf1fe42d860d1e6c7d9f04fec80c4
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-07 14:20:03 -0300

    fix: eslint fix

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,13 +1,13 @@
+import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { getMonthCanceledOrdersAmount } from '@/api/get-month-canceled-orders-amount'
-import { useQuery } from '@tanstack/react-query'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function MonthCanceledOrdersAmountCard() {
-  const {data: monthCanceledOrdersAmount } = useQuery({
+  const { data: monthCanceledOrdersAmount } = useQuery({
     queryFn: getMonthCanceledOrdersAmount,
-    queryKey: ['metrics', 'month-canceled-orders-amount']
+    queryKey: ['metrics', 'month-canceled-orders-amount'],
   })
 
   return (
@@ -19,7 +19,7 @@ export function MonthCanceledOrdersAmountCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-      {monthCanceledOrdersAmount && (
+        {monthCanceledOrdersAmount && (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
