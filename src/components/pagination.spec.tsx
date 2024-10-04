7a80bedd6d8cc8631addcc3963b05b95f2ce7e3e
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-10-04 07:09:01 -0300

    test: pagination component wip

--- src/components/pagination.spec.tsx
+++ src/components/pagination.spec.tsx
@@ -1,32 +1,39 @@
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Pagination } from './pagination'
 
+const onPageChangeCallback = vi.fn()
+
 describe('Pagination', () => {
-  it('Should display the right amount of pages and results', () => {
+  it('should display the right amount of pages and results', () => {
     const wrapper = render(
-      <Pagination 
+      <Pagination
         pageIndex={0}
         totalCount={200}
         perPage={10}
         onPageChange={() => {}}
-      />
+      />,
     )
-
     expect(wrapper.getByText('Página 1 de 20')).toBeInTheDocument()
     expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
   })
-
-  it('Should be able to navigate to the next page', () => {
+  
+  it('should be able to navigate to the next page', async () => {
+    const user = userEvent.setup()
     const wrapper = render(
-      <Pagination 
+      <Pagination
         pageIndex={0}
         totalCount={200}
         perPage={10}
-        onPageChange={() => {}}
-      />
+        onPageChange={onPageChangeCallback}
+      />,
     )
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Próxima página',
+    })
 
-    expect(wrapper.getByText('Página 1 de 20')).toBeInTheDocument()
-    expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
+    await user.click(nextPageButton)
+    
+    expect(onPageChangeCallback).toHaveBeenCalledWith(1)
   })
 })
